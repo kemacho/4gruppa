cd84ccb03ce69ab470656f6e4dad3ab755359b76
kemacho <99050452+kemacho@users.noreply.github.com>
2024-04-10 09:57:19 +0000

    added methods init and more

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 class TRoad:
-  pass
+
+  def __init__(self, road, n):
+    self.road = list(road)
+    self.n = n
+
+  def simulate_traffic(self):
+    pass
+
+  def __light_change(self):
+    pass
+  
+  def __move_car(self):
+    pass
+  
